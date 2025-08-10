c4ff19a62f104245a089f5a3cdacc00ddbfbf6ed
finnweidner76@gmail.com <finnweidner76@gmail.com>
2025-08-10 12:12:22 +0000

    simulation works with maybe better efficiency
    FossilOrigin-Name: 705fc0b8bd5b8e516b284e1da6e9349592c9127ff3b58dfe23466edb77225f60

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::collections::HashMap;
 use std::fs::{self, OpenOptions, write};
+use std::hash::Hash;
 use std::io::Write;
 
 #[derive(Debug, Clone)]
 struct Camel {
     color: Color,
-    upper_camel: Vec<Color>,
-    pos: u32,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -26,57 +25,21 @@ struct Dice {
 
 #[derive(Debug, Clone)]
 struct Configuration {
+    pos_color_map: HashMap<u32, Vec<Color>>,
     dice_queue: Vec<Dice>,
     available_colours: Vec<Color>,
-    camel_map: HashMap<Color, Camel>,
 }
 
 fn main() {
     const COUNT_ALL: u32 = 5 * 4 * 3 * 2 * 3_u32.pow(5);
-    let mut init_camels: HashMap<Color, Camel> = HashMap::new();
-    init_camels.insert(
-        Color::Blue,
-        Camel {
-            color: Color::Blue,
-            upper_camel: vec![Color::Yellow],
-            pos: 1,
-        },
-    );
-    init_camels.insert(
-        Color::Green,
-        Camel {
-            color: Color::Green,
-            upper_camel: vec![Color::White],
-            pos: 2,
-        },
-    );
-    init_camels.insert(
-        Color::Orange,
-        Camel {
-            color: Color::Orange,
-            upper_camel: vec![],
-            pos: 3,
-        },
-    );
-    init_camels.insert(
-        Color::White,
-        Camel {
-            color: Color::White,
-            upper_camel: vec![],
-            pos: 2,
-        },
-    );
-    init_camels.insert(
-        Color::Yellow,
-        Camel {
-            color: Color::Yellow,
-            upper_camel: vec![],
-            pos: 1,
-        },
-    );
+    let init_pos_color_map: HashMap<u32, Vec<Color>> = HashMap::from([
+        (1, vec![Color::Blue, Color::Green]),
+        (2, vec![Color::Yellow, Color::White]),
+        (3, vec![Color::Orange]),
+    ]);
 
     let init_conf = Configuration {
-        camel_map: init_camels,
+        pos_color_map: init_pos_color_map,
         dice_queue: Vec::new(),
         available_colours: vec![
             Color::Blue,
@@ -87,9 +50,23 @@ fn main() {
         ],
     };
 
-    let mut configs: Vec<Configuration> = vec![init_conf];
+    let configs = simulate_round(init_conf);
 
-    for _ in 0..5 {
+    for (i, conf) in configs.iter().enumerate() {
+        let mut file = OpenOptions::new().append(true).open("test.txt").unwrap();
+        let _ = writeln!(file, "Config {i}:\n{conf:?}");
+    }
+
+    //TODO:calc camel leaderboard position
+    //TODO:calc probabilities
+}
+
+//simulate round (remaining dice throws)
+fn simulate_round(init_config: Configuration) -> Vec<Configuration> {
+    let amount_throws = init_config.available_colours.len();
+    let mut configs: Vec<Configuration> = vec![init_config];
+
+    for _ in 0..amount_throws {
         let mut new_confs: Vec<Configuration> = Vec::new();
         for conf in &configs {
             for color in &conf.available_colours {
@@ -100,29 +77,11 @@ fn main() {
         configs = new_confs;
     }
 
-    for (i, conf) in configs.iter().enumerate() {
-        let mut file = OpenOptions::new().append(true).open("test.txt").unwrap();
-        let _ = writeln!(file, "Config {i}:\n{:?}", conf);
-    }
-
-    //TODO:calc camel leaderboard position
-    //TODO:calc probabilities
+    configs
 }
 
 ///returns all possible 3 dice values as new Configurations
 fn simulate_dice_throw(conf: &Configuration, dice_color: &Color) -> Vec<Configuration> {
-    let init_camel_pos = conf
-        .camel_map
-        .values()
-        .filter_map(|camel| {
-            if camel.color == *dice_color {
-                Some(camel.pos)
-            } else {
-                None
-            }
-        })
-        .next();
-    assert!(init_camel_pos.is_some());
     let mut confs: Vec<Configuration> = Vec::new();
 
     for n in 1..=3 {
@@ -133,30 +92,38 @@ fn simulate_dice_throw(conf: &Configuration, dice_color: &Color) -> Vec<Configur
         });
 
         new_conf.available_colours.retain(|c| *c != *dice_color);
+        let old_pos = *new_conf
+            .pos_color_map
+            .iter()
+            .filter_map(|(pos, colors)| {
+                if colors.contains(dice_color) {
+                    Some(pos)
+                } else {
+                    None
+                }
+            })
+            .next()
+            .unwrap();
 
-        new_conf
-            .camel_map
-            .entry(*dice_color)
-            .and_modify(|c| c.pos += n);
+        let old_pos_camels = new_conf.pos_color_map.get_mut(&old_pos).unwrap();
 
-        //remove from camles on which the camle sits
-        for camle in new_conf.camel_map.values_mut() {
-            camle.upper_camel.retain(|col| col != dice_color);
+        let mut moving_camels: Vec<Color> = Vec::new();
+        while let Some(last) = old_pos_camels.pop() {
+            if last != *dice_color {
+                moving_camels.push(last);
+            } else {
+                moving_camels.push(last);
+                break;
+            }
         }
 
-        let up_camels = new_conf
-            .camel_map
-            .get(dice_color)
-            .unwrap()
-            .upper_camel
-            .clone();
+        moving_camels.reverse();
+        let new_pos = old_pos + n;
 
-        //move up camles
-        for up_color in up_camels {
-            new_conf
-                .camel_map
-                .entry(up_color)
-                .and_modify(|c| c.pos += n);
+        if let Some(camels_new_pos) = new_conf.pos_color_map.get_mut(&new_pos) {
+            camels_new_pos.append(&mut moving_camels);
+        } else {
+            new_conf.pos_color_map.insert(new_pos, moving_camels);
         }
 
         confs.push(new_conf);
